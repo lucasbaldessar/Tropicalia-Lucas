95e20ba2605db11edb2a96af386417def1b4ec78
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-04-01 08:26:05 -0300

    lucas

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
    let tamnhoAtualFonte = 1;
 
   aumentaFonteBotao.addEventListener('click', function(){
-    tamnhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamnhoAtualFonte}rem`;
-});
+      tamnhoAtualFonte += 0.1;
+      document.body.style.fontSize = `${tamnhoAtualFonte}rem`;
+   });
 };
